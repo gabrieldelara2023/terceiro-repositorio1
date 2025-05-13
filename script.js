c2b2d9846c5ec397661fe4f447de81c1073635c9
gabrieldelara2023 <144928062+gabrieldelara2023@users.noreply.github.com>
2025-05-13 11:06:37 -0300

    craseeeeeeeeeeeee

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
 })
